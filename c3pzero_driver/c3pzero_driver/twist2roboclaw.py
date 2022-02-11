523bae16ae20bfbda3d6018319fd4f9b1195dc19
Marq <marq.razz@gmail.com>
2022-02-10 18:52:16 -0700

    add odom timer callback

--- c3pzero_driver/c3pzero_driver/twist2roboclaw.py
+++ c3pzero_driver/c3pzero_driver/twist2roboclaw.py
@@ -24,17 +24,18 @@ class RoboclawTwistSubscriber(Node):
 
     def __init__(self):
         super().__init__('roboclaw_twist_subscriber')
+        cmd_vel_topic = 'c3pzero/cmd_vel'
         self.subscription = self.create_subscription(
             Twist,
-            'cmd_vel',
-            self.listener_callback,
+            cmd_vel_topic,
+            self.twist_listener_callback,
             10)
         self.subscription  # prevent unused variable warning
 
         self.rc = roboclaw_3.Roboclaw("/dev/ttyACM0",115200)
 
         if not self.rc.Open():
-            print("failed to open port")
+            self.get_logger().error('failed to open port')
         self.rc_address = 0x80
 
         version = self.rc.ReadVersion(self.rc_address)
@@ -43,15 +44,28 @@ class RoboclawTwistSubscriber(Node):
         else:
             self.get_logger().info('Roboclaw version: %s' % repr(version[1]))
         
-        self.get_logger().info('Init complete')
+        self.create_timer(0.02, self.odom_callback)
 
-    def listener_callback(self, msg):
+        self.get_logger().info('Init complete, listening for twist commands on topic: %s' % cmd_vel_topic)
+
+    def twist_listener_callback(self, msg):
         # self.get_logger().info('X_vel: %f, Z_rot: %f' % msg.linear.x, msg.angular.z)
 
         right_wheel = 0.4*msg.linear.x + (msg.angular.z * .54)/2
         left_wheel = 0.4*msg.linear.x - (msg.angular.z * .54)/2
+        print(int(18000 * right_wheel))
         self.rc.SpeedM1(self.rc_address, int(18000 * right_wheel))
         self.rc.SpeedM2(self.rc_address, int(18000 * left_wheel))
+
+    def odom_callback(self):
+        right_wheel_enc = self.rc.ReadEncM1(self.rc_address)
+        # print(right_wheel_enc[1])
+        left_wheel_enc  = self.rc.ReadEncM2(self.rc_address)
+        right_wheel_speed = self.rc.ReadSpeedM1(self.rc_address)
+        left_wheel_speed  = self.rc.ReadSpeedM2(self.rc_address)
+        # print(left_wheel_speed)
+
+        self.get_logger().info('Pos: %i, %i, Vel: %i, %i' % (right_wheel_enc[1], left_wheel_enc[1], right_wheel_speed[1], left_wheel_speed[1]))
 
 def main(args=None):
     rclpy.init(args=args)
