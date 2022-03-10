b740dcfb2de54db6aba4569f905ad56e74ef00d2
Marq <marq.razz@gmail.com>
2022-03-09 20:33:24 -0700

    configure f710 joystick for teleop

--- c3pzero_driver/launch/teleop.launch.py
+++ c3pzero_driver/launch/teleop.launch.py
@@ -12,21 +12,24 @@ def generate_launch_description():
     config_filepath = launch.substitutions.LaunchConfiguration('config_filepath')
 
     return launch.LaunchDescription([
+        launch.actions.DeclareLaunchArgument('joy_vel', default_value='c3pzero/cmd_vel'),
         launch.actions.DeclareLaunchArgument('joy_config', default_value='f710'),
         launch.actions.DeclareLaunchArgument('joy_dev', default_value='/dev/input/js0'),
         launch.actions.DeclareLaunchArgument('config_filepath', default_value=[
             launch.substitutions.TextSubstitution(text=os.path.join(
-                get_package_share_directory('teleop_twist_joy'), 'config', '')),
+                get_package_share_directory('c3pzero_driver'), 'config', '')),
             joy_config, launch.substitutions.TextSubstitution(text='.config.yaml')]),
 
         launch_ros.actions.Node(
             package='joy', executable='joy_node', name='joy_node',
             parameters=[{
                 'dev': joy_dev,
-                'deadzone': 0.1,
+                'deadzone': 0.3,
                 'autorepeat_rate': 20.0,
             }]),
         launch_ros.actions.Node(
             package='teleop_twist_joy', executable='teleop_node',
-            name='teleop_twist_joy_node', parameters=[config_filepath]),
+            name='teleop_twist_joy_node', parameters=[config_filepath],
+            remappings={('/cmd_vel', launch.substitutions.LaunchConfiguration('joy_vel'))},
+            ),
     ])
